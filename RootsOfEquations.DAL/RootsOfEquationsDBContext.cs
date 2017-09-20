950e872c51b7871764ac981cbe8ef99322ad7172
AdamBorowiecki <adamborowiecki1990@gmail.com>
2017-09-20 11:18:18 +0200

    First setup of DBContext

---
+++ RootsOfEquations.DAL/RootsOfEquationsDBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
+
+namespace RootsOfEquations.DAL
+{
+    public class RootsOfEquationsDBContext //: DbContext
+    {
+        /*public RootsOfEquationsDBContext()
+        {
+
+        }
+
+        public RootsOfEquationsDBContext(DbContextOptions<RootsOfEquationsDBContext> options)
+            : base(options)
+        {
+
+        }*/
+    }
+}
---
+++ RootsOfEquationsCalculator/DAL/RootsOfEquationsDBContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Data.Entity;
+using Microsoft.Data.Entity.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootsOfEquationsCalculator.DAL
+{
+    public class RootsOfEquationsDBContext : DbContext
+    {
+        public RootsOfEquationsDBContext()
+        {
+
+        }
+
+        public RootsOfEquationsDBContext(DbContextOptions<RootsOfEquationsDBContext> options)
+            : base(options)
+        {
+
+        }
+
+        /*public DbSet<RootsCalculationResult> rootsCalculationResults { get; set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase());
+
+            services.AddMvc();
+        }*/
+
+    }
+}
